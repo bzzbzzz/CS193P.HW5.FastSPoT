aa751b10df4aa64b79936e8ce78354c4ceb39ab9
fxv <unmeldungen@gmail.com>
2013-02-27 22:14:18 +0100

    Initial Commit

---
+++ CS193P.HW5.FastSPoT/FSTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FSTAppDelegate.h
+//  CS193P.HW5.FastSPoT
+//
+//  Created by Felix Vigl on 27.02.13.
+//  Copyright (c) 2013 Felix Vigl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FSTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CS193P.HW5.FastSPoT/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CS193P.HW5.FastSPoT
+//
+//  Created by Felix Vigl on 27.02.13.
+//  Copyright (c) 2013 Felix Vigl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FSTAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([FSTAppDelegate class]));
+	}
+}
