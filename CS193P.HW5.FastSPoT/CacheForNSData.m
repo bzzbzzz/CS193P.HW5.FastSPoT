ad15b1b8cc02b87c1626d2b0466841f1edc695b1
fxv <unmeldungen@gmail.com>
2013-02-28 15:12:45 +0100

    ...

--- CS193P.HW5.FastSPoT/CacheForNSData.m
+++ CS193P.HW5.FastSPoT/CacheForNSData.m
@@ -21,8 +21,52 @@
 @implementation CacheForNSData
 
 
-#define MAX_CACHE_SIZE 3 *1048576	// [ MB ]
-#define MAX_CACHE_IPAD_FACTOR 4		// factor of ^
+#define MAX_CACHE_SIZE			3	*1048576	// [ MB ]
+#define MAX_CACHE_IPAD_FACTOR	4
+
+
+//----------------------------------------------------------------
+# pragma mark   -   cache methods
+//----------------------------------------------------------------
+
+
+- (BOOL)cacheData:(NSData *)data withIdentifier:(NSString *)identifier
+{
+	NSArray *directoryContents = [self.fileManager contentsOfDirectoryAtURL:[NSURL fileURLWithPath:self.cacheDirectory]
+												 includingPropertiesForKeys:@[NSURLContentAccessDateKey]
+																	options:NSDirectoryEnumerationSkipsHiddenFiles error:nil];
+	
+	NSMutableArray *contentFilesSortedByLastAccessDateNewerToOlderArray = [[directoryContents sortedArrayUsingComparator: ^(NSURL* url1, NSURL* url2) {
+		NSDate *date1 = [url1 resourceValuesForKeys:@[NSURLContentAccessDateKey] error:nil][NSURLContentAccessDateKey];
+		NSDate *date2 = [url2 resourceValuesForKeys:@[NSURLContentAccessDateKey] error:nil][NSURLContentAccessDateKey];
+		
+		return [date2 compare: date1];
+		
+	}] mutableCopy];
+	
+	while (self.cacheSize >= self.maxCacheSize && directoryContents.count > 0) {
+		NSLog(@"cachSize: %i , maxCacheSize: %i , directoryContents.count: %lu ",self.cacheSize, self.maxCacheSize, (unsigned long)directoryContents.count);
+        
+		NSError *hardimitzn = nil;
+		if (![self.fileManager removeItemAtURL:[contentFilesSortedByLastAccessDateNewerToOlderArray lastObject] error:&hardimitzn]) NSLog(@"%@", [hardimitzn localizedDescription]);
+		else [contentFilesSortedByLastAccessDateNewerToOlderArray removeLastObject];
+	}
+	
+	NSString * targetFilePath = [self.cacheDirectory stringByAppendingPathComponent:identifier];
+	
+	return [data writeToFile:targetFilePath atomically:YES];
+}
+
+
+- (NSData *)dataInCacheForIdentifier:(NSString *)identifier
+{
+	NSString * targetFilePath = [self.cacheDirectory stringByAppendingPathComponent:identifier];
+	NSData *data;
+	if ([self.fileManager fileExistsAtPath:targetFilePath]) {
+		data = [[NSData alloc] initWithContentsOfFile:targetFilePath];
+	}
+	return data;
+}
 
 
 //----------------------------------------------------------------
@@ -84,50 +128,6 @@
 {
 	if (!_runningOniPad) _runningOniPad = ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad);
 	return _runningOniPad;
-}
-
-
-//----------------------------------------------------------------
-# pragma mark   -   cache methods
-//----------------------------------------------------------------
-
-
-- (BOOL)cacheData:(NSData *)data withIdentifier:(NSString *)identifier
-{
-	NSArray *directoryContents = [self.fileManager contentsOfDirectoryAtURL:[NSURL fileURLWithPath:self.cacheDirectory]
-																   includingPropertiesForKeys:@[NSURLContentAccessDateKey]
-																					  options:NSDirectoryEnumerationSkipsHiddenFiles error:nil];
-	
-	NSMutableArray *contentFilesSortedByLastAccessDateNewerToOlderArray = [[directoryContents sortedArrayUsingComparator: ^(NSURL* url1, NSURL* url2) {
-		NSDate *date1 = [url1 resourceValuesForKeys:@[NSURLContentAccessDateKey] error:nil][NSURLContentAccessDateKey];
-		NSDate *date2 = [url2 resourceValuesForKeys:@[NSURLContentAccessDateKey] error:nil][NSURLContentAccessDateKey];
-		
-		return [date2 compare: date1];
-
-	}] mutableCopy];	
-	
-	while (self.cacheSize >= self.maxCacheSize && directoryContents.count > 0) {
-		NSLog(@"cachSize: %i , maxCacheSize: %i , directoryContents.count: %lu ",self.cacheSize, self.maxCacheSize, (unsigned long)directoryContents.count);
-        
-		NSError *hardimitzn = nil;
-		if (![self.fileManager removeItemAtURL:[contentFilesSortedByLastAccessDateNewerToOlderArray lastObject] error:&hardimitzn]) NSLog(@"%@", [hardimitzn localizedDescription]);
-		else [contentFilesSortedByLastAccessDateNewerToOlderArray removeLastObject];
-	}
-	
-	NSString * targetFilePath = [self.cacheDirectory stringByAppendingPathComponent:identifier];
-	
-	return [data writeToFile:targetFilePath atomically:YES];
-}
-
-
-- (NSData *)dataInCacheForIdentifier:(NSString *)identifier
-{
-	NSString * targetFilePath = [self.cacheDirectory stringByAppendingPathComponent:identifier];
-	NSData *data;
-	if ([self.fileManager fileExistsAtPath:targetFilePath]) {
-		data = [[NSData alloc] initWithContentsOfFile:targetFilePath];
-	}
-	return data;
 }
 
 
